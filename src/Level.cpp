ca6b2dd3e6ef48d2793cfcf0a18a3b827bae2ced
Kebigon <git@kebigon.xyz>
2018-08-12 18:51:22 +0900

    Stop level scrolling when the border of the level is reached

--- src/Level.cpp
+++ src/Level.cpp
@@ -7,7 +7,7 @@
 
 Level::Level(const uint8_t *levelData, const uint8_t *entitiesData)
 	: levelData(levelData)
-	, width(levelData)
+	, width(pgm_read_byte(levelData))
 	,entities(pgm_read_byte(entitiesData))
 {
 	uint8_t* currentAddress = entitiesData;
@@ -47,6 +47,7 @@ void Level::draw() const
 
 void Level::update()
 {
+	// Stop level scrolling
 	if (divideBy8(startX) + 16 != width)
 		startX++;
 }
